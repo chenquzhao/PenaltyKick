6a3d769bfd8559c17349c9258e2213597d44387e
Michael <chec98qu@gmail.com>
2019-05-24 23:02:38 +0800

    implement penalty kick to the target

--- app/src/main/java/com/example/a26083/penalty/ThirdActivity.java
+++ app/src/main/java/com/example/a26083/penalty/ThirdActivity.java
@@ -11,6 +11,9 @@ import static android.view.GestureDetector.*;
 
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 
 public class ThirdActivity extends AppCompatActivity implements OnGestureListener,OnDoubleTapListener{
@@ -18,7 +21,10 @@ public class ThirdActivity extends AppCompatActivity implements OnGestureListene
     private static ImageView textView;
     private GestureDetectorCompat GestureDetect;
     private ImageView soccer;
-
+    private String value1, value2, value3;
+//    private int sp = Integer.parseInt(value1);
+//    private int sa = Integer.parseInt(value2);
+//    private int ms = Integer.parseInt(value3);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,16 +34,15 @@ public class ThirdActivity extends AppCompatActivity implements OnGestureListene
         soccer = (ImageView) findViewById(R.id.soccer);
         soccer.setImageResource(R.drawable.soccer);
 
-
         TextView txt3 = (TextView) findViewById(R.id.txt3);
         TextView txt4 = (TextView) findViewById(R.id.txt4);
         TextView txt5 = (TextView) findViewById(R.id.txt5);
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
-            String value = extras.getString("pow");
-            String value2 = extras.getString("acc");
-            String value3 = extras.getString("min");
-            txt3.setText(value);
+            value1 = extras.getString("pow");
+            value2 = extras.getString("acc");
+            value3 = extras.getString("min");
+            txt3.setText(value1);
             txt4.setText(value2);
             txt5.setText(value3);
     }
@@ -49,7 +54,6 @@ public class ThirdActivity extends AppCompatActivity implements OnGestureListene
         GestureDetect.setOnDoubleTapListener(this);
 
 }
-
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
@@ -71,8 +75,8 @@ public class ThirdActivity extends AppCompatActivity implements OnGestureListene
     @Override
     public boolean onDoubleTapEvent(MotionEvent e) {
 
-        soccer.setTranslationX(10);
-        soccer.setTranslationY(10);
+        soccer.setTranslationX(0);
+        soccer.setTranslationY(0);
         return false;
     }
 
@@ -96,8 +100,8 @@ public class ThirdActivity extends AppCompatActivity implements OnGestureListene
 
     @Override
     public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-        soccer.setTranslationX(-300);
-        soccer.setTranslationY(-300);
+//        soccer.setTranslationX(-300);
+//        soccer.setTranslationY(-300);
 
         return false;
     }
@@ -110,6 +114,47 @@ public class ThirdActivity extends AppCompatActivity implements OnGestureListene
 
     @Override
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        float minMove = 120;
+        float minVelocity = 0;
+        float beginX = e1.getX();
+        float endX = e2.getX();
+        float beginY = e1.getY();
+        float endY = e2.getY();
+
+        int one = 200+ (int) (Math.random() * 400);
+        int two = 50+ (int) (Math.random() * 550);
+        int three = 50+ (int) (Math.random() * 550);
+        int four = 200 + (int) (Math.random() * 400);
+        int five = 200+ (int) (Math.random() * 400);
+
+
+
+//        if(beginX - endX > minMove && Math.abs(velocityX) > minVelocity&& Math.abs(beginY - endY)<minMove){
+//            soccer.setTranslationX(-one);
+//            Toast.makeText(this,"left",Toast.LENGTH_SHORT).show();
+//        }
+         if(beginX - endX > minMove && Math.abs(velocityX) > minVelocity&&beginY - endY > minMove && Math.abs(velocityY) > minVelocity){
+            soccer.setTranslationX(-two);
+            soccer.setTranslationY(-one);
+            Toast.makeText(this,"leftup",Toast.LENGTH_SHORT).show();
+        }
+//        else if(endX - beginX > minMove && Math.abs(velocityX) > minVelocity&&Math.abs(beginY - endY)<minMove){
+//            soccer.setTranslationX(two);
+//            Toast.makeText(this,"right",Toast.LENGTH_SHORT).show();
+//        }
+        else if(endX - beginX > minMove && Math.abs(velocityX) > minVelocity&&beginY - endY > minMove && Math.abs(velocityY) > minVelocity){
+            soccer.setTranslationX(three);
+            soccer.setTranslationY(-four);
+            Toast.makeText(this,"rightup",Toast.LENGTH_SHORT).show();
+        }
+        else if(beginY - endY > minMove && Math.abs(velocityY) > minVelocity&&Math.abs(beginX - endX)<minMove){
+            soccer.setTranslationY(-five);
+            Toast.makeText(this,"up",Toast.LENGTH_SHORT).show();
+        }
+//        else if(endY - beginY > minMove && Math.abs(velocityY) > minVelocity){
+//            soccer.setTranslationY(300);
+//            Toast.makeText(this,"down",Toast.LENGTH_SHORT).show();
+//        }
 
         return false;
     }
